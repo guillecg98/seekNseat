dad89c5b888bc5abf453649756e83f547b9fadad
Guille Cosano <i62cogag@uco.es>
2021-07-01 11:42:46 +0200

    added find method to repository interface

--- apps/api/src/business/domain/respository/businesses.ts
+++ apps/api/src/business/domain/respository/businesses.ts
@@ -1,7 +1,8 @@
-import { Business } from "../model";
+import { Business, BusinessId } from "../model";
 
 export interface Businesses {
     save(business: Business): void;
+    find(businessId: BusinessId): Promise<Business | null>;
 }
 
 export const BUSINESSES = 'BUSINESSES'
