934a3e79444a10c6d7a9a8bc2328cb6e36f685f9
Guille Cosano <i62cogag@uco.es>
2021-07-13 17:32:27 +0200

    config react-native-paper

--- apps/mobile/src/main.tsx
+++ apps/mobile/src/main.tsx
@@ -2,7 +2,7 @@ import 'react-native-gesture-handler';
 
 import React from 'react';
 import { AppRegistry } from 'react-native';
-import { Provider as PaperProvider } from 'react-native-paper';
+import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
 
 import App from './app/App';
 
