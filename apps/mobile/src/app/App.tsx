cc909fdbd9fe3283a49b793d8e07e8326a518cb6
Guille Cosano <i62cogag@uco.es>
2021-07-13 17:32:59 +0200

    config main app routes with react navigation

--- apps/mobile/src/app/App.tsx
+++ apps/mobile/src/app/App.tsx
@@ -3,32 +3,21 @@ import { createStackNavigator } from '@react-navigation/stack';
 import React, { useEffect } from 'react';
 import { Button, Text, View} from 'react-native';
 
-import { RegisterBusinessScreen, TypeOfUserScreen } from './screens';
+import { ReservationButton } from './components';
+import { BusinessScreen, RegisterBusinessScreen, TypeOfUserScreen } from './screens';
 
 const Stack = createStackNavigator();
-
-const HomeScreen = ({ navigation }) => {
-  return(
-  <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-    <Text>Home Screen</Text>
-    <Button
-      title="to register"
-      onPress={() => navigation.navigate('Register')}/>
-  </View>
-  )
-}
 
 const App = () => {
 
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Register" component={RegisterBusinessScreen} />
+      <Stack.Navigator initialRouteName="Initial">
         <Stack.Screen name="Initial" component={TypeOfUserScreen} />
+        <Stack.Screen name="Register" component={RegisterBusinessScreen} />
+        <Stack.Screen name="Business" component={BusinessScreen} />
       </Stack.Navigator>
     </NavigationContainer>
-    // <HomeScreen />
   );
 }
 
