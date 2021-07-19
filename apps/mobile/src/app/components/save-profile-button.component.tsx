19a683f1cc7e65b85e89ccb556778cd1591c5493
Guille Cosano <i62cogag@uco.es>
2021-07-19 14:08:24 +0200

    new components for edit profile screen

---
+++ apps/mobile/src/app/components/save-profile-button.component.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import { Button } from 'react-native-elements';
+import { baseProps } from 'react-native-gesture-handler/lib/typescript/handlers/gestureHandlers';
+
+const styles = StyleSheet.create({
+    button: {
+        fontSize: 20,
+        color: '#596275',
+    },
+})
+
+type Props = {
+    onPress: any;
+}
+
+export const SaveProfileButton = (props: Props) => {
+    return (
+        <Button
+            titleStyle={styles.button}
+            title='SAVE'
+            type='clear'
+            onPress={props.onPress} />
+    )
+}
