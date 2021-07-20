33e0d8800ac16470fdb63506422269d752cefa6b
Guille Cosano <i62cogag@uco.es>
2021-07-20 18:00:53 +0200

    url fail

--- apps/mobile/src/app/requests/queries/get-categories.request.ts
+++ apps/mobile/src/app/requests/queries/get-categories.request.ts
@@ -2,7 +2,8 @@ import axios from "axios";
 
 export const getCategories = async () => {
     try {
-        return await axios.get(`${process.env.RN_REMOTE_URL}/api/categories`);
+        return await axios.get(`http://localhost:3333/api/categories
+        `);
     } catch (e) {
         console.error(e);
     }
