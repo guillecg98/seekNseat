56870d95a016765133c25d7ae9922bd54523db30
Guille Cosano <i62cogag@uco.es>
2021-07-22 14:42:05 +0200

    added booking DTOs

---
+++ libs/contracts/src/lib/booking/create-booking.dto.ts
@@ -0,0 +1,9 @@
+export class CreateBookingDTO {
+    id: string;
+    userId: string;
+    businessId: string;
+    //bookingTime: DateTime;
+    numberOfFoodies: number;
+    bookingState: string;
+    //noShow: boolean;
+}
---
+++ libs/contracts/src/lib/booking/edit-booking.dto.ts
@@ -0,0 +1,4 @@
+export class EditBookingDTO {
+    bookingState: string;
+    //noShow: boolean;
+}
