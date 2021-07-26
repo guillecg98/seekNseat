bed0ccca5a040767d25087b2c076a4a9fdbc47e3
Guille Cosano <i62cogag@uco.es>
2021-07-26 19:52:58 +0200

    new queries for managing bookings for users and businesses

--- apps/api/src/booking/infrastructure/booking.module.ts
+++ apps/api/src/booking/infrastructure/booking.module.ts
@@ -4,16 +4,22 @@ import { EventSourcingModule } from "event-sourcing-nestjs";
 
 import { AuthModule } from "../../auth/auth.module";
 import { DatabaseModule } from "../../database/database.module";
+import { GetBookingByUserHandler, GetBookingsByBusinessHandler, GetBookingsHanlder } from "../application";
 import { RequestBookingHandler } from "../application/command/request-booking.hanlder";
 import { BookingProviders } from "./booking.provider";
 import { BookingController } from "./controller/booking.controller";
 import { BookingWasRequestedProjection } from "./read-model/projection/booking-was-requested.projection";
+import { BookingMapper } from "./repository";
 
 const CommandHandlers = [
     RequestBookingHandler,
 ];
 
-const QueryHandlers = [];
+const QueryHandlers = [
+    GetBookingsHanlder,
+    GetBookingByUserHandler,
+    GetBookingsByBusinessHandler,
+];
 
 const ProjectionHandlers = [
     BookingWasRequestedProjection,
@@ -27,7 +33,7 @@ const ProjectionHandlers = [
         ...QueryHandlers,
         ...ProjectionHandlers,
         ...BookingProviders,
-        //BookingMapper,
+        BookingMapper,
     ]
 })
 
--- apps/api/src/booking/infrastructure/controller/booking.controller.ts
+++ apps/api/src/booking/infrastructure/controller/booking.controller.ts
@@ -1,9 +1,12 @@
-import { BadRequestException, Body, ClassSerializerInterceptor, Controller, Post, UseInterceptors } from "@nestjs/common";
+import { BadRequestException, Body, ClassSerializerInterceptor, Controller, Get, Param, Post, Res, UseInterceptors } from "@nestjs/common";
 import { CommandBus, QueryBus } from "@nestjs/cqrs";
-import { ApiResponse, ApiTags } from "@nestjs/swagger";
+import { ApiOperation, ApiResponse, ApiTags } from "@nestjs/swagger";
 import { BookingDTO, CreateBookingDTO } from "@seekNseat/contracts";
+import { Response } from 'express';
 
-import { RequestBookingCommand } from "../../application";
+import { GetBookingsByBusinessQuery, GetBookingsByUserQuery, GetBookingsQuery, RequestBookingCommand } from "../../application";
+import { BookingView } from "../read-model/shcema/booking.schema";
+import { BookingMapper } from "../repository";
 
 @ApiTags('bookings')
 @Controller('bookings')
@@ -12,6 +15,7 @@ export class BookingController {
     constructor(
         private commandBus: CommandBus,
         private queryBus: QueryBus,
+        private bookingMapper: BookingMapper,
     ) {}
 
     @Post()
@@ -31,6 +35,77 @@ export class BookingController {
                 throw new BadRequestException(e.message);
             } else {
                 throw new BadRequestException('Server error');
+            }
+        }
+    }
+
+    @Get()
+    @ApiResponse({ status: 200, description: 'List Bookings'})
+    @ApiResponse({ status: 404, description: 'Bookings not found'})
+    async findAll(@Res({passthrough: true}) res: Response): Promise<BookingDTO[]> {
+        try {
+            const bookings = await this.queryBus.execute<GetBookingsQuery, BookingView[]>(
+                new GetBookingsQuery()
+            )
+
+            res.setHeader('X-Total-Count', bookings.length)
+
+            return bookings.map(this.bookingMapper.viewToDto);
+        } catch(e) {
+            if(e instanceof Error) {
+                throw new BadRequestException(e.message);
+            } else {
+                throw new BadRequestException('Server Error')
+            }
+        }
+    }
+
+    @Get(':userId/bookings')
+    @ApiOperation({summary: 'Get bookings of user'})
+    @ApiResponse({ status: 200, description: 'List bookings of one user'})
+    @ApiResponse({ status: 404, description: 'Bookings not found'})
+    async findAllByUser(
+        @Res({passthrough: true}) res: Response,
+        @Param('userId') userId: string
+        ): Promise<BookingDTO[]> {
+        try {
+            const bookings = await this.queryBus.execute<GetBookingsByUserQuery, BookingView[]>(
+                new GetBookingsByUserQuery(userId)
+            )
+
+            res.setHeader('X-Total-Count', bookings.length)
+
+            return bookings.map(this.bookingMapper.viewToDto);
+        } catch(e) {
+            if(e instanceof Error) {
+                throw new BadRequestException(e.message);
+            } else {
+                throw new BadRequestException('Server Error')
+            }
+        }
+    }
+
+    @Get(':businessId/bookings')
+    @ApiOperation({summary: 'Get bookings of business'})
+    @ApiResponse({ status: 200, description: 'List bookings of one business'})
+    @ApiResponse({ status: 404, description: 'Bookings not found'})
+    async findAllByBusiness(
+        @Res({passthrough: true}) res: Response,
+        @Param('businessId') businessId: string
+        ): Promise<BookingDTO[]> {
+        try {
+            const bookings = await this.queryBus.execute<GetBookingsByBusinessQuery, BookingView[]>(
+                new GetBookingsByBusinessQuery(businessId)
+            )
+
+            res.setHeader('X-Total-Count', bookings.length)
+
+            return bookings.map(this.bookingMapper.viewToDto);
+        } catch(e) {
+            if(e instanceof Error) {
+                throw new BadRequestException(e.message);
+            } else {
+                throw new BadRequestException('Server Error')
             }
         }
     }
