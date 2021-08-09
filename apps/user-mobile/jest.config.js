afa2f8d9af5ed1f51e27b73eb3d024315dda4927
Guille Cosano <i62cogag@uco.es>
2021-08-09 18:25:27 +0200

    feat: created new mobile app for users and settedp up react-native with nx  workspace

---
+++ apps/user-mobile/jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  displayName: 'user-mobile',
+  preset: 'react-native',
+  moduleFileExtensions: ['ts', 'js', 'html', 'tsx', 'jsx'],
+  setupFilesAfterEnv: ['<rootDir>/test-setup.ts'],
+  transform: {
+    '\\.(js|ts|tsx)$': require.resolve('react-native/jest/preprocessor.js'),
+    '^.+\\.(bmp|gif|jpg|jpeg|mp4|png|psd|svg|webp)$': require.resolve(
+      'react-native/jest/assetFileTransformer.js'
+    ),
+  },
+};
