796c4b8d89a3317e41a5cc620c5bf7f4346c2358
Guille Cosano <i62cogag@uco.es>
2021-08-11 20:36:07 +0200

    chore(firebase): set up google login with firebase project

--- apps/user-mobile/android/app/src/main/java/com/usermobile/MainApplication.java
+++ apps/user-mobile/android/app/src/main/java/com/usermobile/MainApplication.java
@@ -1,4 +1,4 @@
-package com.usermobile;
+package es.uco.aulasoftwarelibre.seeknseat.client;
 
 import android.app.Application;
 import android.content.Context;
@@ -62,7 +62,7 @@ public class MainApplication extends Application implements ReactApplication {
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.usermobile.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("es.uco.aulasoftwarelibre.seeknseat.client.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
