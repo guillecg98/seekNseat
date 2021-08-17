84c2cbea230ac808c9a65b80c231a405bfed7efe
Guille Cosano <i62cogag@uco.es>
2021-08-17 20:22:42 +0200

    feat: recieving user from backend when user loggin with google

--- apps/user-mobile/src/app/auth/navigation/auth-provider.tsx
+++ apps/user-mobile/src/app/auth/navigation/auth-provider.tsx
@@ -3,7 +3,10 @@ import {
   GoogleSignin,
   statusCodes,
 } from '@react-native-google-signin/google-signin';
+import axios from 'axios';
 import React, { createContext, useState } from 'react';
+
+import { googleLogin } from '../../user/requests';
 
 interface IUser {
   id: string;
@@ -13,6 +16,15 @@ interface IUser {
   familyName: string;
   givenName: string;
 }
+
+const initialUser: IUser = {
+  id: '',
+  name: '',
+  email: '',
+  photo: '',
+  familyName: '',
+  givenName: '',
+};
 
 interface IBookingData {
   foodies: number;
@@ -24,20 +36,13 @@ const initialBookingData: IBookingData = {
   time: new Date(),
 };
 
-const initialUser: IUser = {
-  id: '',
-  name: '',
-  email: '',
-  photo: '',
-  familyName: '',
-  givenName: '',
-};
-
 export const AuthContext = createContext({
   bookingData: initialBookingData,
   setBookingData: undefined,
   user: initialUser,
   setUser: undefined,
+  userId: '',
+  setUserId: undefined,
   login: () => {
     console.log('defaultValue');
   },
@@ -47,8 +52,10 @@ export const AuthContext = createContext({
 });
 
 export const AuthProvider: React.FC = ({ children }) => {
-  const [bookingData, setBookingData] = useState<IBookingData>(initialBookingData);
+  const [bookingData, setBookingData] =
+    useState<IBookingData>(initialBookingData);
   const [user, setUser] = useState<IUser>();
+  const [userId, setUserId] = useState('')
 
   return (
     <AuthContext.Provider
@@ -57,22 +64,33 @@ export const AuthProvider: React.FC = ({ children }) => {
         setBookingData,
         user,
         setUser,
+        userId,
+        setUserId,
         login: async () => {
           try {
             const { idToken, user } = await GoogleSignin.signIn();
             const googleCredential =
               auth.GoogleAuthProvider.credential(idToken);
             await auth().signInWithCredential(googleCredential);
+
+            const response = await axios.post(
+              'http://localhost:3333/api/google-login',
+              {
+                token: googleCredential.token,
+              }
+            );
+            setUserId(response.data._id);
             setUser(user);
+            console.log(user);
           } catch (error) {
             if (error.code === statusCodes.SIGN_IN_CANCELLED) {
-              // user cancelled the login flow
+              throw new Error('User Canceled loggin');
             } else if (error.code === statusCodes.IN_PROGRESS) {
               // operation (e.g. sign in) is in progress already
             } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
-              // play services not available or outdated
+              throw new Error('Play Services not available or outdated');
             } else {
-              // some other error happened
+              console.error(error);
             }
           }
         },
