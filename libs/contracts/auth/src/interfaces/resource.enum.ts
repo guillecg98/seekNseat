5368c167bbbc9b6d9b818666a812fd4bdfb02886
Guille Cosano <i62cogag@uco.es>
2021-08-19 11:42:30 +0200

    refactor: modify files location

---
+++ libs/contracts/auth/src/interfaces/resource.enum.ts
@@ -0,0 +1,4 @@
+export enum Resource {
+  Business = 'business',
+  Booking = 'booking',
+}
---
+++ libs/contracts/auth/src/interfaces/role.enum.ts
@@ -0,0 +1,5 @@
+export enum Role {
+  Admin = 'ROLE_ADMIN',
+  User = 'ROLE_USER',
+  BusinessOwner = 'ROLE_BUSINESS_OWNER',
+}
