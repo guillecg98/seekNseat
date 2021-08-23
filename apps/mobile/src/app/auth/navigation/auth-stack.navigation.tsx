49b5b719826d065bf05e0775e7ad206f3c57a8f2
Guille Cosano <i62cogag@uco.es>
2021-08-23 20:24:29 +0200

    feat: implemented google auth to create user

--- apps/mobile/src/app/auth/navigation/auth-stack.navigation.tsx
+++ apps/mobile/src/app/auth/navigation/auth-stack.navigation.tsx
@@ -10,20 +10,17 @@ const Stack = createStackNavigator();
 export const AuthStack = () => {
   useEffect(() => {
     GoogleSignin.configure({
-      webClientId:
-        '844656111334-uem0fq79h5qtd11uje1r6olquv53cssg.apps.googleusercontent.com', //process.env not working
+      webClientId: process.env.GOOGLE_AUTH_CLIENT_ID,
     });
   }, []);
 
   return (
     <Stack.Navigator
-      screenOptions={{headerShown: false,}}
-      initialRouteName='SignIn'>
+      screenOptions={{ headerShown: false }}
+      initialRouteName="SignIn"
+    >
       <Stack.Screen name="SignIn" component={SignInScreen} />
-      <Stack.Screen
-        name="Register"
-        component={BusinessRegisterScreen}
-      />
+      <Stack.Screen name="Register" component={BusinessRegisterScreen} />
     </Stack.Navigator>
   );
 };
