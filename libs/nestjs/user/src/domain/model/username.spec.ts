47690935f33016c19e2847995371c0c1bf40b5e9
Guille Cosano <i62cogag@uco.es>
2021-08-25 18:09:10 +0200

    fix: allow google name

--- libs/nestjs/user/src/domain/model/username.spec.ts
+++ libs/nestjs/user/src/domain/model/username.spec.ts
@@ -4,11 +4,6 @@ describe('Username', () => {
   it('should have email format', () => {
     expect(Username.fromString('guillecosano42@gmail.com').value).toBe('guillecosano42@gmail.com');
   });
-  it('should not have spaces', () => {
-    expect(() => {
-      Username.fromString('user name');
-    }).toThrow();
-  });
 
   it('should not be empty', () => {
     expect(() => {
