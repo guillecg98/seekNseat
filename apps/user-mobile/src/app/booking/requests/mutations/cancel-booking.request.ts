e09e017d71c206870fe467b80aff8b3dbbdfc1d6
Guille Cosano <i62cogag@uco.es>
2021-08-26 13:11:45 +0200

    cs

--- apps/user-mobile/src/app/booking/requests/mutations/cancel-booking.request.ts
+++ apps/user-mobile/src/app/booking/requests/mutations/cancel-booking.request.ts
@@ -15,7 +15,7 @@ export const cancelBooking = async (
       },
       {
         headers: { Authorization: `Bearer ${token}` },
-      },
+      }
     );
   } catch (e) {
     console.log(e);
--- apps/user-mobile/src/app/user/requests/queries/get-businesses.request.ts
+++ apps/user-mobile/src/app/user/requests/queries/get-businesses.request.ts
@@ -2,10 +2,12 @@ import axios from 'axios';
 
 export const getBusinesses = async (category: string, token: string) => {
   try {
-    const url = category ? `http://localhost:3333/api/businesses?category=`+category : 'http://localhost:3333/api/businesses';
+    const url = category
+      ? `http://localhost:3333/api/businesses?category=` + category
+      : 'http://localhost:3333/api/businesses';
     // return await axios.get(`${process.env.RN_REMOTE_URL}/api/businesses`);
     return await axios.get(url, {
-      headers: { "Authorization": `Bearer ${token}` },
+      headers: { Authorization: `Bearer ${token}` },
     });
   } catch (e) {
     console.error(e);
